8a2c25611f06ac4687bac1889f45f30a00c3a8a5
ThanhKhiet <thanhkhiet@ThanhKhiets-MacBook-Pro.local>
2014-07-04 20:24:56 +0700

    Initial Commit

---
+++ ButtonLeftRight/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ButtonLeftRight
+//
+//  Created by ThanhKhiet on 7/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
